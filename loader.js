275ef8661548fb5c752629f78fe8073f69654181
steep-47 <2157528478@qq.com>
2026-08-19 06:55:13 +0800

    接入单API可见诊断模块

--- loader.js
+++ loader.js
@@ -1,6 +1,7 @@
 import './index.js';
 import './scripts/runtime/memoryContentRules.js';
 import './scripts/runtime/singleApiGuard.js';
+import './scripts/runtime/singleApiDiagnostics.js';
 import './scripts/ui/pinchZoom.js';
 import './scripts/ui/personTableSplit.js';
 import './scripts/ui/apiModeToggle.js';
