b99f559c1872bdb0447dfce08540b5a199e9c3cb
steep-47 <2157528478@qq.com>
2026-08-19 16:09:18 +0800

    Load non-invasive API toggle on restored original core

--- loader.js
+++ loader.js
@@ -2,3 +2,4 @@ import './index.js';
 import './scripts/runtime/memoryContentRules.js';
 import './scripts/ui/pinchZoom.js';
 import './scripts/ui/personTableSplit.js';
+import './scripts/ui/apiModeToggle.js';
