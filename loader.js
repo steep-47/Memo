b6252625d6a3324f38b31206d344c141ee28e31c
steep-47 <2157528478@qq.com>
2026-08-19 20:46:03 +0800

    Load safe runtime mode guard

--- loader.js
+++ loader.js
@@ -1,6 +1,7 @@
 import './index.js';
 import './scripts/runtime/memoryContentRules.js';
+import './scripts/runtime/modeRuntimeControl.js?v=20260819-2048';
 import './scripts/ui/pinchZoom.js';
 import './scripts/ui/personTableSplit.js';
-import './scripts/ui/apiModeToggle.js';
+import './scripts/ui/apiModeToggle.js?v=20260819-2048';
 import './scripts/ui/fillStatusColor.js';
