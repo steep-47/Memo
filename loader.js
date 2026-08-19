093481db1234bb3fa63159ab6059adcd4f81efb7
steep-47 <2157528478@qq.com>
2026-08-19 18:52:39 +0800

    Remove experimental API bridge and toast hooks

--- loader.js
+++ loader.js
@@ -1,7 +1,5 @@
-import './scripts/runtime/apiModeBridge.js';
 import './index.js';
 import './scripts/runtime/memoryContentRules.js';
 import './scripts/ui/pinchZoom.js';
 import './scripts/ui/personTableSplit.js';
 import './scripts/ui/apiModeToggle.js';
-import './scripts/ui/fillStatusToast.js';
