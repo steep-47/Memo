a2a99f484589c47fbc86a373d6cc3cf1b6528bb4
steep-47 <2157528478@qq.com>
2026-08-19 23:45:43 +0800

    Refresh fixed tableEdit protocol modules

--- loader.js
+++ loader.js
@@ -1,9 +1,9 @@
 import './index.js';
 import './scripts/runtime/memoryContentRules.js';
 import './scripts/runtime/modeRuntimeControl.js?v=20260819-2048';
-import './scripts/runtime/singleApiPromptRestore.js?v=20260819-2215';
+import './scripts/runtime/singleApiPromptRestore.js?v=20260819-2345';
 import './scripts/runtime/singleApiFinish.js?v=20260819-2325';
-import './scripts/runtime/singleApiDiagnostic.js?v=20260819-2320';
+import './scripts/runtime/singleApiDiagnostic.js?v=20260819-2345';
 import './scripts/ui/pinchZoom.js';
 import './scripts/ui/personTableSplit.js';
 import './scripts/ui/apiModeToggle.js?v=20260819-2048';
