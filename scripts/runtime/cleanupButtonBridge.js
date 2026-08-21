b6aaf39794cbfde2c1190eafd21bd247293ad0b7
steep-47 <2157528478@qq.com>
2026-08-21 16:23:21 +0800

    Align cleanup bridge cache with memo74

--- scripts/runtime/cleanupButtonBridge.js
+++ scripts/runtime/cleanupButtonBridge.js
@@ -1,4 +1,4 @@
-import { runStableCleanup } from './stableTableCleanup.js?v=memo73';
+import { runStableCleanup } from './stableTableCleanup.js?v=memo74';
 
 const INSTALL_FLAG = '__memoCleanupButtonBridgeInstalled';
 
