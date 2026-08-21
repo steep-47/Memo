415a2f37b0d94cb8bc086ba2da30ec93cd880dfe
steep-47 <2157528478@qq.com>
2026-08-21 11:46:33 +0800

    fix: keep cleanup bridge on shared v67 module instance

--- scripts/runtime/cleanupButtonBridge.js
+++ scripts/runtime/cleanupButtonBridge.js
@@ -1,4 +1,4 @@
-import { runStableCleanup } from './stableTableCleanup.js?v=memo65';
+import { runStableCleanup } from './stableTableCleanup.js?v=memo67';
 
 const INSTALL_FLAG = '__memoCleanupButtonBridgeInstalled';
 
