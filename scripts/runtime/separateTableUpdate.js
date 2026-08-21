a4db19b5f568d76b8d9f3d2eae241133aa6a10af
steep-47 <2157528478@qq.com>
2026-08-21 20:44:21 +0800

    memo83: update scripts/runtime/separateTableUpdate.js

--- scripts/runtime/separateTableUpdate.js
+++ scripts/runtime/separateTableUpdate.js
@@ -4,8 +4,8 @@ import { reloadCurrentChat } from '/script.js';
 import { getTableEditTag, getTablePrompt, getTablePromptByPiece } from '../../index.js';
 import { handleCustomAPIRequest, handleMainAPIRequest } from '../settings/standaloneAPI.js';
 import { updateSystemMessageTableStatus } from '../renderer/tablePushToChat.js';
-import { repairMissingColumnsBeforeCleanup } from './tableStructureRepair.js?v=memo82';
-import { executeMemoTableEdit, saveMemoSnapshot } from './safeTableExecutor.js?v=memo82';
+import { repairMissingColumnsBeforeCleanup } from './tableStructureRepair.js?v=memo83';
+import { executeMemoTableEdit, saveMemoSnapshot } from './safeTableExecutor.js?v=memo83';
 import JSON5 from '../../utils/json5.min.mjs';
 
 const INDEPENDENT_OPERATION_RULES = `# Memo独立记录操作协议
